269826d30b121fb804a7627686741039071fa731
Georg Dangl <georg@dangl.me>
2021-01-02 19:52:25 +0100

    Add project files.

---
+++ Dangl.ClockodoExport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dangl.ClockodoExport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
