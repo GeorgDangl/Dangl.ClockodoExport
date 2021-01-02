5bfd041cc878190a73b6de367e6116ee40c05b5c
Georg Dangl <georg@dangl.me>
2021-01-02 20:12:16 +0100

    Add ApiExportOptions

---
+++ src/Dangl.ClockodoExport/ApiExportOptions.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace Dangl.ClockodoExport
+{
+    public class ApiExportOptions
+    {
+        [Option('t', "token", Required = true, HelpText = "Your Clockodo API Token")]
+        public string SevDeskApiToken { get; set; }
+
+        [Option('u', "user-email", Required = true, HelpText = "Email for the Clockodo account")]
+        public string UserEmail { get; set; }
+    }
+}
