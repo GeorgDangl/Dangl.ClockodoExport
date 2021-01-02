79f65daa8739912ccaa830c4d4f280559886b8e9
Georg Dangl <georg@dangl.me>
2021-01-02 20:14:43 +0100

    Add Main method

--- src/Dangl.ClockodoExport/Program.cs
+++ src/Dangl.ClockodoExport/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Threading.Tasks;
 
 namespace Dangl.ClockodoExport
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HeadingInfo.Default.WriteMessage("Visit https://www.dangl-it.com to find out more about this exporter");
+            HeadingInfo.Default.WriteMessage("This generator is available on GitHub: https://github.com/GeorgDangl/Dangl.ClockodoExport");
+            HeadingInfo.Default.WriteMessage($"Version {VersionInfo.Version}");
+            await Parser.Default.ParseArguments<ApiExportOptions>(args)
+                .MapResult(async options =>
+                {
+                    try
+                    {
+                        var exporter = new Exporter(options);
+                        await exporter.ExportClockodoDataAndWriteToDiskAsync().ConfigureAwait(false);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                },
+                errors =>
+                {
+                    Console.WriteLine("Could not parse CLI arguments");
+                    return Task.CompletedTask;
+                }).ConfigureAwait(false);
         }
     }
 }
